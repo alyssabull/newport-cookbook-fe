6b36c110bea5d428674c27ef568236da64cc8f88
alyssabull <alyssabull@gmail.com>
2024-01-02 12:28:22 -0700

    More refactoring

--- src/App.js
+++ src/App.js
@@ -4,18 +4,10 @@ import './App.css';
 const App = () => {
   // const [allRecipes, setAllRecipes] = useState(null);
   const getRecipes = async () => {
-    // return fetch(`${process.env.REACT_APP_DB_URL}/getmovies`)
-    // .then(response => {
-    //   if (!response.ok) {
-    //     throw Error(`Failed to fetch movies`)
-    //   }
-    //   setAllRecipes(response.json())
-    // })
-    // .catch(err => console.log(err))
-    const response = fetch(`${process.env.REACT_APP_DB_URL}/getmovies`);
-    const result = (await response).json();
-    console.log(result)
-    return result;
+    return fetch(`${process.env.REACT_APP_DB_URL}/getmovies`)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
   };
 
   // const displayRecipes = () => {

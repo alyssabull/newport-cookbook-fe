980664eb5e6fb1aad33b5f10590f4d08d62e569d
alyssabull <alyssabull@gmail.com>
2024-01-02 12:25:58 -0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [allRecipes, setAllRecipes] = useState(null);
-  const getRecipes = () => {
-    return fetch(`${process.env.REACT_APP_DB_URL}/getmovies`)
-    .then(response => {
-      if (!response.ok) {
-        throw Error(`Failed to fetch movies`)
-      }
-      setAllRecipes(response.json())
-    })
-    .catch(err => console.log(err))
+  // const [allRecipes, setAllRecipes] = useState(null);
+  const getRecipes = async () => {
+    // return fetch(`${process.env.REACT_APP_DB_URL}/getmovies`)
+    // .then(response => {
+    //   if (!response.ok) {
+    //     throw Error(`Failed to fetch movies`)
+    //   }
+    //   setAllRecipes(response.json())
+    // })
+    // .catch(err => console.log(err))
+    const response = fetch(`${process.env.REACT_APP_DB_URL}/getmovies`);
+    const result = (await response).json();
+    console.log(result)
+    return result;
   };
 
-  const displayRecipes = () => {
-    if (allRecipes) {
-      console.log(allRecipes)
+  // const displayRecipes = () => {
+  //   if (allRecipes) {
+  //     console.log(allRecipes)
       // return allRecipes.map((recipe) => {
       //   return (
       //     <div key={recipe.id}>{recipe.movieName}</div>
       //   )
       // })
-    };
-  };
+  //   };
+  // };
 
   return (
     <div className="App">
       <h1>Newport Cookbook</h1>
       <button onClick={getRecipes}>Get Recipes</button>
       <div>
-        {displayRecipes()}
+        {/* {displayRecipes()} */}
       </div>
     </div>
   );

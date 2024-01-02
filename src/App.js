8c03527b9627c7da4b6333f1dbfd825a379ee7e4
alyssabull <alyssabull@gmail.com>
2024-01-02 12:20:41 -0700

    Display results

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
-  // const [allRecipes, setAllRecipes] = useState(null);
+  const [allRecipes, setAllRecipes] = useState(null);
   const getRecipes = () => {
     return fetch(`${process.env.REACT_APP_DB_URL}/getmovies`)
     .then(response => {
       if (!response.ok) {
         throw Error(`Failed to fetch movies`)
       }
-      console.log(response.json())
+      setAllRecipes(response.json())
     })
     .catch(err => console.log(err))
   };
 
-  // const displayRecipes = () => {
-    // if (allRecipes) {
-      // console.log(allRecipes)
+  const displayRecipes = () => {
+    if (allRecipes) {
+      console.log(allRecipes[0].data)
       // return allRecipes.map((recipe) => {
       //   return (
       //     <div key={recipe.id}>{recipe.movieName}</div>
       //   )
       // })
-    // };
-  // };
+    };
+  };
 
   return (
     <div className="App">
       <h1>Newport Cookbook</h1>
       <button onClick={getRecipes}>Get Recipes</button>
       <div>
-        {/* {displayRecipes()} */}
+        {displayRecipes()}
       </div>
     </div>
   );
